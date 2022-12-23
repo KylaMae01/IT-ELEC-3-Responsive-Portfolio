07efc619b934dabd1e3374d40078d934ed7039d1
Kyla mae Marbella <91838633+KylaMae01@users.noreply.github.com>
2022-12-24 01:22:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle) {
