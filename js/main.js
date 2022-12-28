02d98df2c200e6e474a04a9ddb4f025451a830a9
Kyla mae Marbella <91838633+KylaMae01@users.noreply.github.com>
2022-12-28 22:37:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
